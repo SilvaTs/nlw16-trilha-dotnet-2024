03210bef6071bc729f7fabbd184440d3f3cf1496
Daniel Silva <danielsilvatsi@outlook.com>
2024-07-08 21:20:47 -0300

    feat: implement registerUseCase

---
+++ src/Journey.Application/UseCases/Trips/Register/RegisterTripUseCase.cs
@@ -0,0 +1,30 @@
+﻿using Journey.Communication.Requests;
+using Journey.Exception.ExceptionsBase;
+
+namespace Journey.Application.UseCases.Trips.Register;
+
+public class RegisterTripUseCase
+{
+    public void Execute(RequestRegisterTripJson request)
+    {
+        Validate(request);
+    }
+
+    private void Validate(RequestRegisterTripJson request)
+    {
+        if (string.IsNullOrWhiteSpace(request.Name))
+        {
+            throw new JourneyException("Nome não pode ser vazio");
+        }
+
+        if (request.StartDate.Date < DateTime.UtcNow.Date)
+        {
+            throw new JourneyException("Viagem não pode ser registrada para uma data passada.");
+        }
+
+        if (request.EndDate.Date < request.StartDate.Date)
+        {
+            throw new JourneyException("A viagem deve terminar após a data de início.");
+        }
+    }
+}
